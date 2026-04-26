20a869c4b4704db8371528ea10b5c466ca3edcb5
Durga Kishore <chitturidurgakishore@gmail.com>
2026-04-26 21:11:44 +0530

    Added colors to show prority

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=1.1">
+    <script src="{{ asset('js/app.js') }}?v=1.1" defer></script>
 </head>
 
 <body>
